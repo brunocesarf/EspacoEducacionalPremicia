74839d292b0dc64c64e64b8438dda58f258f1c26
Bruno Cesar Firmino <77680695+brunocesarf@users.noreply.github.com>
2021-01-19 16:46:16 -0300

    Código PHP

---
+++ Premicia.php
@@ -0,0 +1,290 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title>Espaço educaciona Premicia</title>
+    <style>
+        #main {
+            margin-left: 120px
+        }
+
+        .w3-row-padding img {
+            margin-bottom: 12px
+        }
+
+        .w3-sidebar {
+            width: 80px;
+            background: #FFFFFF;
+        }
+
+
+        body,
+        h1,
+        h2,
+        h3,
+        h4,
+        h5,
+        h6 {
+            font-family: "Montserrat", sans-serif
+        }
+
+        @media only screen and (max-width: 600px) {
+            #main {
+                margin-left: 0
+            }
+        }
+    </style>
+
+</head>
+
+<body class="w3-white">
+
+<div class="w3-top">
+<nav class="w3-drodown-hover w3-center w3-mobile" style="background: #FFFAFA">
+  <a href="#home" class="w3-bar-item w3-button w3-padding-large w3-hover-black w3-hover-text-white w3-text-black">
+            <i class="fa fa-home w3-xxlarge"></i>
+            <p>HOME</p>
+        </a>
+        <a href="#sobre" class="w3-bar-item w3-button w3-padding-large w3-hover-black w3-hover-text-white w3-text-black">
+            <i class="fa fa-user w3-xxlarge"></i>
+            <p>SOBRE</p>
+        </a>
+        <a href="#gradeC" class="w3-bar-item w3-button w3-padding-large w3-hover-black w3-hover-text-white w3-text-black">
+            <i class="fa fa-pencil-square-o w3-xxlarge"></i>
+            <p>PACOTES</p>
+        </a>
+        <a href="#docentes" class="w3-bar-item w3-button w3-padding-large w3-hover-black w3-hover-text-white w3-text-black">
+            <i class="fa fa-address-card w3-xxlarge"></i>
+            <p>DOCENTES</p>
+        </a>
+        <a href="#contato" class="w3-bar-item w3-button w3-padding-large w3-hover-black w3-hover-text-white w3-text-black">
+            <i class="fa fa-envelope w3-xxlarge"></i>
+            <p>CONTATO</p>
+        </a>
+</nav>
+</div>
+
+<div class="w3-sidebar w3-bar-block w3-hide-small">
+
+        <img src="IMG/logoCab.jpg" alt="Logo Espaço educacional Premícias." class="w3-image" width="100%">
+        <a href="https://www.facebook.com/espacoeducacionalprimicias" class="w3-bar-item w3-button w3-padding-large w3-hover-black w3-hover-text-white w3-text-black">
+            <i class="fa fa-facebook-f w3-large"></i>
+        </a>
+        
+        <a href="https://www.instagram.com/eeprimicias/?hl=pt-br" class="w3-bar-item w3-button w3-padding-large w3-hover-black w3-hover-text-white w3-text-black">
+            <i class="fa fa-instagram w3-large"></i>
+        </a>
+        
+        <a href="https://wa.me/message/Y4Y57N6OHIFPO1" class="w3-bar-item w3-button w3-padding-large w3-hover-black w3-hover-text-white w3-text-black">
+            <i class="fa fa-whatsapp w3-large"></i>
+        </a>
+        <a href="https://api.whatsapp.com/send?phone=5512988145447" class="w3-bar-item w3-button w3-padding-large w3-hover-black w3-hover-text-white w3-text-black">
+            <i class="fa fa-whatsapp w3-large"></i>
+        </a>
+        
+    </div>
+            
+    <div class="w3-top w3-hide-large w3-hide-medium" >
+        <nav class="w3-bar w3-black w3-opacity w3-hover-opacity-off w3-center w3-small">
+            <a href="#home" class="w3-bar-item w3-button" style="width:25% ">HOME</a>
+            <a href="#sobre" class="w3-bar-item w3-button" style="width:25% ">SOBRE</a>
+            <a href="#contato" class="w3-bar-item w3-button" style="width:23% ">CONTATO</a>
+        </nav>
+    </div>
+    
+
+    
+    <div class="w3-padding-large" id="main">
+        
+        <header class="w3-container w3-padding-32 w3-center " id="home">
+            <h1>
+                <img src="IMG/logoCab.jpg" alt="Logo Espaço educacional Premícias." class="w3-image" width="40%">
+            </h1>
+            
+            <h2>
+                <p>Atendimento de Matemática, Português, Reforço escolar, Alfabetização, Raciocínio Lógico, preparação para vestibular. </p>
+            </h2>
+        </header>
+        
+
+        
+        <div class="w3-content  w3-padding-64" id="sobre">
+            <h2 class="w3-text-black">Sobre os Cursos</h2>
+            <hr style="width:200px" class="w3-opacity">
+            <ul class="w3-ul w3-hoverable w3-center w3-white">
+                <li class="w3-xlarge w3-padding-32" style="background: #970">O nosso espaço  tem como objetivo capacitar o aluno para:</li>
+                <li class="w3-padding-16">Lingua Portuguesa:Interpretação de texto; Figuras de linguagem; Síntese e resumo;
+Denotação e conotação;Gêneros textuais; Funções da linguagem; Norma culta e popular, variação linguística entre outros conteúdos. </li>
+                <li class="w3-padding-16">Matemática: Operações Básicas, Potenciação e Radiciação, Expressões Numéricas, Múltiplos, Divisores, MDC e MMC
+Operações Básicas com Polinômios, Frações, Decimais e Dízimasentre outros conteúdos </li>
+                <li class="w3-padding-16">O reforço escolar: Uma forma de auxiliar o aluno a compreender melhor a matéria passada na escola, e assim ajudar na fixação do 
+                    conteúdo em sua mente, não decorando o que está sendo ensinado, mas sim, entendendo e buscando aplicar de alguma forma, em sua vida ou em atividades diárias.</li>
+                <li class="w3-padding-16">Raciocínio lógico: O termo diz respeito a capacidade de organizar dados através de premissas válidas para chegar a uma conclusão. Ele é fundamental para preparar os seus 
+                    filhos para situações mais complexas</li>
+                <li class="w3-padding-16">Alfabetização e letramento: A alfabetização é o processo de ensino e aprendizagem de um sistema linguístico e da forma como usá-lo para se comunicar com a sociedade. 
+                    O letramento é o estado que um indivíduo ou grupo social alcança depois de se familiarizar com a escrita e a leitura, possuindo uma maior experiência para desenvolver as práticas do seu uso nos mais diversos contextos sociais.  </li>
+                <li class="w3-padding-16">Preparação para o vestibular: Apoio para desenvolver tecnicas para a preparação do vestibular isso requer bastante dedicação e disciplina.</li>
+            </ul>
+            <div class="w3-row w3-padding-16 w3-section w3-light-grey w3-center">
+                <div class="w3-third w3-section">
+                    <span class="w3-xlarge">1 hora por dia </span>
+                    <br> Aula
+                </div>
+                <div class="w3-third w3-section">
+                    <span class="w3-xlarge">2 vezes por semana</span>
+                    <br> Aulas Semanais
+                </div>
+                <div class="w3-third w3-section">
+                    <span class="w3-xlarge"> 8 Aulas no mês </span>
+                    <br> Conteúdo de Português e Matemática
+                </div>
+            </div>
+        </div>
+        
+
+
+        
+        <div class=" w3-content w3-padding-64" id="gradeC">
+            <h2 class="w3-text-black">Pacotes </h2>
+            <hr style="width:200px" class="w3-opacity">
+
+            <div class="w3-row w3-row-padding">
+                <div class="w3-third w3-margin-bottom">
+                    <ul class="w3-ul w3-white w3-center w3-opacity w3-hover-opacity-off">
+                        <li class="w3-xlarge w3-padding-32" style="background: #970">Trimestral</li>
+                        <li class="w3-padding-16">Conteúdos de Português e Matemática</li>
+                        <li class="w3-padding-16">3 Aulas de reposição</li>
+                        <li class="w3-padding-16">Parcela em 3x  </li>
+    </div>
+
+                <div class="w3-third">
+                    <ul class="w3-ul w3-white w3-center w3-opacity w3-hover-opacity-off">
+                        <li class="w3-xlarge w3-padding-32" style="background: #970">Semestral </li>
+                        <li class="w3-padding-16">Conteúdos de Português e Matemática</li>
+                        <li class="w3-padding-16">6 Aulas de reposição</li>
+                        <li class="w3-padding-16">Parcela em 6x</li>
+    </div>
+
+                <div class="w3-third">
+                    <ul class="w3-ul w3-white w3-center w3-opacity w3-hover-opacity-off">
+                        <li class="w3-xlarge w3-padding-32" style="background: #970">Anual</li>
+                        <li class="w3-padding-16">Conteúdos de Português e Matemática</li>
+                        <li class="w3-padding-16">12 Aulas de reposição</li>
+                        <li class="w3-padding-16">Parcela em 10x</li>
+                       
+                            
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="w3-thid">
+                <ul class="w3-ul w3-white w3-center">
+                    <li class="w3-padding-16"><h3>Consulte para outros Pacotes!</h3></li>
+        </div>
+
+        <div class="w3-padding-64 w3-content w3-display-container" id="docentes">
+            <h2 class="w3-text-black">Docentes</h2>
+            <hr style="width:200px" class="w3-opacity">
+        
+            <div class="w3-row w3-row-padding slides w3-center">
+                <div class="w3-half">
+                    <img src="IMG/docente.jpeg" style="width:50%" class="w3-opacity w3-hover-opacity-off">
+                </div>
+                    <div class="w3-half">
+                    <p>
+                        <h4 style="color:#960;">Celia Fernandes Rodrigues</h4>
+                        <h6 style="color:#960;">Formaçaõ Acadêmica</h6>
+                        <p>Graduada Pedagogia pela Faculdade de Anhanguera (2016).<br>Pós-Graduada em Psicopedagogia Instituciona e Clinica pela Faculdade Ceel (2019). <br>Pós-Graduação em Educação Especial DI pela Faculdade Ceel (2020).</p>
+                        <h6 style="color:#960;">Atuação Profissional</h6>
+                        <p>Professora do ensino de educação infantil. Ecola Mariano Ribeiro.<br>Professora de ensino fundamental. Escola Silva Silveira.</p>
+                    </p>
+                </div>
+            </div>
+            <div class="w3-row w3-row-paddig slides w3-center">    
+                <div class="w3-half">
+                    <img src="IMG/docente2.jpeg" style="width:50%" class="w3-opacity w3-hover-opacity-off">
+            </div>
+                    <div class="w3-half">
+                        <p>
+                            <h4 style="color:#960;">Scarlet Flavia Monteiro de Siqueira</h4>
+                            <h6 style="color:#960;">Formaçaõ Acadêmica</h6>
+                            <p>Graduada em Pedagogia pela <br>Faculdade Anhanguera (2015).<br>Pós-Graduada em Pedagogia Hospitalar, Psicopedagogia  Clínica, Gestão educacional, pela Faculdade Campos de Eliseu (2016/2020).</p>
+                            <h6 style="color:#960;">Atuação Profissional</h6>
+                            <p>Professora do ensino fundamental na Escola Tito Máximo no ano de 2015 - 2016<br>Experiência na área de Educação</p>
+                        </p>
+                     </div>   
+                </div>
+            
+            
+            <button class="w3-button w3-display-left" style="background: #FFFFF" onclick="plusDivs(-1)">&#10094;</button>
+            <button class="w3-button w3-display-right" style="background: #FFFFF" onclick="plusDivs(1)">&#10095;</button>
+        </div>
+
+        <script>
+            var slideIndex = 1;
+            showDivs(slideIndex);
+            
+            function plusDivs(n) {
+              showDivs(slideIndex += n);
+            }
+            
+            function showDivs(n) {
+              var i;
+              var x = document.getElementsByClassName("slides");
+              if (n > x.length) {slideIndex = 1}    
+              if (n < 1) {slideIndex = x.length}
+              for (i = 0; i < x.length; i++) {
+                 x[i].style.display = "none";  
+              }
+              x[slideIndex-1].style.display = "block";  
+            }
+            </script>
+                
+        <div class="w3-padding-128 w3-content w3-text-grey" id="contato">
+            <h2 class="w3-text-black">Contato</h2>
+            <hr style="width:200px" class="w3-opacity">
+            <div class="w3-section">
+                <p>
+                    <i class="fa fa-map-marker fa-fw w3-text-black w3-xxlarge w3-margin-right"></i> Jacareí, São Paulo, Brasil</p>
+                <p>
+                    <i class="fa fa-phone fa-fw w3-text-black w3-xxlarge w3-margin-right"></i> Telefone: 12 99186-8430 - 12 98814-5447</p>
+                <p>
+                    <i class="fa fa-envelope fa-fw w3-text-black w3-xxlarge w3-margin-right"> </i> Email: eepremicia@gmail.com</p>
+            </div>
+            <br>
+            <p>Entre em contato:</p>
+
+            <form>
+                <p>
+                    <input class="w3-input w3-padding-16" type="text" placeholder="Nome" required name="Nome">
+                </p>
+                <p>
+                    <input class="w3-input w3-padding-16" type="text" placeholder="Email" required name="Email">
+                </p>
+                <p>
+                    <input class="w3-input w3-padding-16" type="text" placeholder="Assunto" required name="Assunto">
+                </p>
+                <p>
+                    <input class="w3-input w3-padding-16" placeholder="Menssagem" required name="Mensagem">
+                </p>
+                <br>
+                <br>
+                <p>
+                    <button class="w3-button w3-light-grey w3-padding-large" type="submit">
+                        <i class="fa fa-paper-plane"></i> Enviar mensagem
+                    </button>
+                </p>
+            </form>
+            <br>
+        </div>
+        
+
+    </div>
+</body>
+
+</html>
